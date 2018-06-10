26e66001fc3c6516eff976ddc4a55aa19a546277
Kimberly Lalmansingh <kimlalman28@gmail.com>
2018-06-10 16:33:49 -0400

    typewriter effect

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 // let skills = ["HTML5", "CSS3", "Javascript", "Node.js", "ExpressJS", "jQuery", "MongoDB", "C++", "PHP", "Bootstrap", "React", "Git/Github"];
 
-
+// Nav-bar 
 $(".section-link").click(function(e) {
     e.preventDefault();
     var section = $(this).attr("href");
@@ -8,3 +8,36 @@ $(".section-link").click(function(e) {
         scrollTop: ($(section).offset().top - 50)
     }, 2000);
 });
+
+var aText = new Array("Kimberly Lalmansingh");
+var iSpeed = 150; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+ 
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+ 
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("hero-name");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+typewriter();
