bbdef45b031bba6b6898f5614573f2247b99c145
Kimberly Lalmansingh <kimlalman28@gmail.com>
2018-06-10 16:04:32 -0400

    new layout fixes

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,6 @@ $(".section-link").click(function(e) {
     e.preventDefault();
     var section = $(this).attr("href");
     $("html, body").animate({
-        scrollTop: ($(section).offset().top - 200)
+        scrollTop: ($(section).offset().top - 50)
     }, 2000);
 });
